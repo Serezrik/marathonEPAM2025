386f698fe5cc2f1d321fac78b207279eec8d51b6
Serezrik <vocanc@gmail.com>
2025-11-08 23:03:25 +0000

    Update S3 bucket and key for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
-    key          = "terraform.tfstate"
+    bucket       = "test-marathonepam2025"
+    key          = "test-marathonepam2025"
     region       = "eu-central-1"
     use_lockfile = true
     encrypt      = true
